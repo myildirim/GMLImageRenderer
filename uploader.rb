9c9590f804d1705f1f6d17cd5be1df0ed7980091
Jamie Wilkinson <jamie@tramchase.com>
2010-07-20 18:58:41 +0000

    clean up & comment the uploader. Output error messages/status codes, and measure the time it takes to do everything

--- uploader.rb
+++ uploader.rb
@@ -1,38 +1,50 @@
 #!/usr/bin/ruby
+# Use our fetched tag data to generate images using GMLImageRenderer.py
+# requires the 'curb' gem, which depends on the libcurl-dev package
+
+require 'yaml'
+require 'benchmark'
 require 'rubygems'
 require 'curb'
-require 'yaml'
 
 prod = true
-#id = ARGV[0] || 18013
 file = ARGV[1] || "output.png"
-#puts "uploading tag id=#{id.inspect} file=#{file.inspect}"
 
+# Parse data compiled by fetch_ga_tags.rb
 ids = YAML.load(File.open('ids.yml').read)
-puts "#{ids.length} tag ids loaded."
+STDOUT.puts "000000book Thumbnail Uploader -- #{ids.length} tag ids loaded."
 
 ids.each do |id|
-  puts "id=#{id}"
   file = "output#{id}.png"
 
-  if File.exists?(file)
-    STDERR.puts "File #{file} exists already, skipping"
+  # Don't clobber existing files unless CLOBBER=1
+  if File.exists?(file) && ENV['CLOBBER'].nil?
+    STDERR.puts "* #{id}: Output file #{file.inspect} exists already, skipping"
     next
   end
 
-  puts `python GMLImageRenderer.py -id #{id} #{file}`
+  # Shell out to python to do the work
+  STDOUT.print "* #{id}: Generating #{file.inspect}... "; STDOUT.flush
+  time = Benchmark.realtime { `python GMLImageRenderer.py -id #{id} #{file}` }
+  STDOUT.print "#{(time*1000).round}ms. "; STDOUT.flush
 
+  # If no file exists, something bad happened
   if !File.exists?(file)
     STDERR.puts "ERROR, failed to generate tag #{id}"
     next
   end
 
-  c = Curl::Easy.new("http://#{prod ? '000000book.com' : 'localhost:3000'}/data/#{id}/thumbnail")
+  # Upload via POST w/ 'image' field
+  hostname = prod ? '000000book.com' : 'localhost:3000'
+  STDOUT.print "\tUploading to #{hostname}... "; STDOUT.flush
+  c = Curl::Easy.new("http://#{hostname}/data/#{id}/thumbnail")
   c.multipart_form_post = true
   post_field = Curl::PostField.content('image', File.open(file).read)
   post_field.remote_file = file
   post_field.content_type = 'application/octet-stream'
-  res = c.http_post(post_field)
-  puts res.inspect
+  time = Benchmark.realtime { res = c.http_post(post_field) }
+  STDOUT.puts "HTTP #{c.response_code}, #{c.body_str.inspect} ... #{(time*1000).round}ms"
 end
-puts "done"
+
+STDOUT.puts "Done."
+exit 0
