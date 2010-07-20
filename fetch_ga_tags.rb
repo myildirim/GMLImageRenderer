b7d49c13e69199f340914fc6c06eaefdce271ccb
Jamie Wilkinson <jamie@tramchase.com>
2010-07-20 00:23:15 -0700

    blow away old ids.yml & support max pages

--- fetch_ga_tags.rb
+++ fetch_ga_tags.rb
@@ -5,17 +5,19 @@ agent = Mechanize.new
 
 tag_ids = []
 page = ENV['PAGE'] || 1
+max_pages = 5 # nil = all
 
+FileUtils.rm_f('ids.yml')
 while true
   doc = agent.get("http://000000book.com/data.xml?app=Graffiti+Analysis+2.0%3A+DustTag&page=#{page}")
   xml = Nokogiri.parse(doc.body)
   ids = (xml/'id').map { |i| i.content.to_i } || []
   puts "page=#{page}, ids=#{ids.inspect}"
-  break if ids.empty?
+  break if ids.empty? || (!max_pages.nil? && page >= max_pages)
   tag_ids += ids
   page += 1
 end
 
-puts File.open('ids.txt', 'w+').write(tag_ids.to_yaml)
+puts File.open('ids.yml', 'w+').write(tag_ids.to_yaml)
 puts "done. ids="
 puts tag_ids.inspect
