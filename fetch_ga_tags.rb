7a4944e6290d6c903f8a3ce8daa65f7e0ca1dd59
Jamie Wilkinson <jamie@tramchase.com>
2010-08-31 09:03:29 -0700

    allow specifying PAGE=100 and MAX_PAGES=50 controls. Send a proper user agent

--- fetch_ga_tags.rb
+++ fetch_ga_tags.rb
@@ -7,23 +7,27 @@ require 'rubygems'
 require 'mechanize'
 
 agent = Mechanize.new
+agent.user_agent = "000000book GMLImageRenderer cronjob"
 
 tag_ids = []
-page = ENV['PAGE'] || 1
-max_pages = ENV['MAX_PAGES'] || 5 # nil = all
+start_page = ENV['PAGE'].nil? ? 1 : ENV['PAGE'].to_i
+max_pages = ENV['MAX_PAGES'].nil? ? 1 : ENV['MAX_PAGES'].to_i  # nil = all
+
+puts "Start page=#{start_page.inspect}, max_pages=#{max_pages}"
 
 FileUtils.rm_f('ids.yml')
+page = start_page
 while true
   url = "http://000000book.com/data.xml?page=#{page}"
   # url += "&app=Graffiti+Analysis+2.0:+DustTag"
   doc = agent.get(url)
   xml = Nokogiri.parse(doc.body)
   ids = (xml/'id').map { |i| i.content.to_i } || []
-  puts "page=#{page}, ids=#{ids.inspect}"
+  puts "page=#{page}, max_pages=#{max_pages}, page+max_pages=#{(page+max_pages).inspect} ids=#{ids.inspect}"
   break if ids.empty?
   tag_ids += ids
   page += 1
-  break if (!max_pages.nil? && page > max_pages)
+  break if (!max_pages.nil? && page >= (start_page+max_pages) )
 end
 
 puts File.open('ids.yml', 'w+').write(tag_ids.to_yaml)
